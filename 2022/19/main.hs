fbbf41ab188a84dfd3aa1a0f242b73c9f3246c65
Gergely Nagy <nagygeri97@gmail.com>
2022-12-28 18:26:13 +0100

    Day 19 Part 2

--- 2022/19/main.hs
+++ 2022/19/main.hs
@@ -45,10 +45,7 @@ convert line = (oreOre, clayOre, (obsiOre, obsiClay), (geoOre, geoObsi))
 type S = (Int, (Int, Int, Int, Int), (Int, Int, Int, Int))
 
 nextStates :: Costs -> S -> [S]
-nextStates (oreOre, clayOre, (obsiOre, obsiClay), (geoOre, geoObsi)) (time, qty@(oreQ, clayQ, obsiQ, geoQ), bot@(oreB, clayB, obsiB, geoB)) 
-    | not (null statesAfterGeoBuy) = statesAfterGeoBuy
-    | not (null statesAfterObsiBuy) = statesAfterObsiBuy
-    | otherwise = statesAfterNoBuy ++ statesAfterOreBuy ++ statesAfterClayBuy
+nextStates (oreOre, clayOre, (obsiOre, obsiClay), (geoOre, geoObsi)) (time, qty@(oreQ, clayQ, obsiQ, geoQ), bot@(oreB, clayB, obsiB, geoB)) = statesAfterNoBuy ++ statesAfterOreBuy ++ statesAfterClayBuy ++ statesAfterObsiBuy ++ statesAfterGeoBuy
     where
         newQ@(nOreQ, nClayQ, nObsiQ, nGeoQ) = (oreQ + oreB, clayQ + clayB, obsiQ + obsiB, geoQ + geoB)
         statesAfterNoBuy = [(time + 1, newQ, bot)]
@@ -62,7 +59,10 @@ iterateStates n costs time states
     | time >= n = states
     | otherwise = iterateStates n costs (time + 1) newStates
     where 
-        newStates = Set.toList . Set.fromList $ (concat . map (nextStates costs) $ states)
+        newStates = take 5000 . sortBy (flip compare `on` scoreState) . concat . map (nextStates costs) $ states
+
+scoreState :: S -> (Int, Int, Int, Int, Int, Int, Int, Int)
+scoreState (time, qty@(oreQ, clayQ, obsiQ, geoQ), bot@(oreB, clayB, obsiB, geoB)) = (geoB, geoQ, obsiB, obsiQ, clayB, clayQ, oreB, oreQ)
 
 initialState :: S
 initialState = (0, (0,0,0,0), (1,0,0,0))
@@ -70,11 +70,14 @@ initialState = (0, (0,0,0,0), (1,0,0,0))
 getMaxGeo :: [S] -> Int
 getMaxGeo = maximum . map (\(_, (_, _, _, g), _) -> g)
 
+getWithMaxGeo :: [S] -> S
+getWithMaxGeo = maximumBy (compare `on` (\(_, (_, _, _, g), _) -> g))
+
 convert2 :: String -> ParsedLine2
 convert2 = convert
 
 solve1 :: [ParsedLine] -> Int
 solve1 lines = sum . zipWith (*) [1..] . map (\cost -> getMaxGeo $ iterateStates 24 cost 0 [initialState]) $ lines 
 
--- solve2 :: [ParsedLine2] -> Int
-solve2 lines = map (\cost -> getMaxGeo $ iterateStates 32 cost 0 [initialState]) . take 3 $ lines 
+solve2 :: [ParsedLine2] -> Int
+solve2 lines = product . map (\cost -> getMaxGeo $ iterateStates 32 cost 0 [initialState]) . take 3 $ lines 
